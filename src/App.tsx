133897db2fe3cce34bd6006967e31eecdc46318f
Liam Hamer <liam.bey80@gmail.com>
2025-06-27 21:11:09 +1200

    tk-9 basic auth 0 support

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@heroui/react";
 import { FeatureFlagsList } from './components/featureFlagsList';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
+import Profile from './components/profile';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-
     <div className=' h-screen grid grid-flow-row grid-cols-5 grid-rows-5 gap-3'>
       <div className='flex flex-col col-start-2 col-end-5 row-start-2 row-span-3 gap-10 text-center'>
         <div className='flex flex-row justify-center'>
@@ -21,7 +23,6 @@ function App() {
           </a>
         </div>
         <h1 className='text-4xl font-bold '>Vite + React</h1>
-        <FeatureFlagsList></FeatureFlagsList>
         <div>
           <Button onPress={() => setCount((count) => count + 1)} color="primary">count is {count}</Button>
           <p>
@@ -32,6 +33,10 @@ function App() {
           Click on the Vite and React logos to learn more
         </p>
       </div>
+      <LoginButton></LoginButton>
+      <LogoutButton></LogoutButton>
+      <FeatureFlagsList></FeatureFlagsList>
+      <Profile></Profile>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,24 @@ import './index.css'
 import App from './App.tsx'
 import { ConfigCatProvider } from "configcat-react";
 import { HeroUIProvider } from '@heroui/react'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const configCatKey = "configcat-sdk-1/TbXdCMZii0WKnjpSmmWNKw/q328WyZg5USrnPo9dLcD_A";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConfigCatProvider sdkKey={configCatKey}>
-      <HeroUIProvider>
-        <App />
-      </HeroUIProvider>
-    </ConfigCatProvider>
+    <Auth0Provider
+      domain="dev-f6vjtxopx4qxeo3j.us.auth0.com"
+      clientId="XOXBVdGsUmhis657stHeCfTwhgcHRU0O"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <ConfigCatProvider sdkKey={configCatKey}>
+        <HeroUIProvider>
+          <App />
+        </HeroUIProvider>
+      </ConfigCatProvider>
+    </Auth0Provider>
   </StrictMode>,
 )
